919dfd6bcb532b4afbc41e4d0646aed4a731c8b9
mayrang <pkss0626@naver.com>
2026-05-27 17:01:49 +0900

    feat: public exports + dist build verified

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+export { useFormDraft } from './useFormDraft';
+export { useFormDraftStatus } from './useFormDraftStatus';
+export { zodAdapter } from './internal/schemaValidation';
+export { localStorageAdapter } from './storage/localStorage';
+export { sessionStorageAdapter } from './storage/sessionStorage';
+export { indexedDBAdapter } from './storage/indexedDB';
+
 export type {
   FormDraftStatus,
   StorageAdapter,
