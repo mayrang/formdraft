1ae855a7a2f79528597bc149e655bad8bdae88b0
mayrang <pkss0626@naver.com>
2026-05-27 17:08:58 +0900

    fix: add eslint-plugin-react-hooks + remove unused act import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 2020, sourceType: 'module', ecmaFeatures: { jsx: true } },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react-hooks'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
--- src/rhf/__tests__/useFormDraftRHF.test.tsx
+++ src/rhf/__tests__/useFormDraftRHF.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
